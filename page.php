a9bd687d4d8dd13d186bb992a04374510e256e68
whitebearcode <whitebear@msn.com>
2024-07-10 21:06:00 +0800

    v1.0.2 release #20240709

---
+++ page.php
@@ -0,0 +1,30 @@
+<?php
+get_header(); ?>
+
+<div class="bgallery_fl_main_content_wrap">
+				
+				<div class="bgallery_fl_gallery_single">
+					<div class="container">
+					<div>
+						<div class="bgallery_fl_about">
+							
+							<div class="about_us sticky_sidebar">
+								<div class="about_us_in">
+									<h2><span style="font-weight:bold;letter-spacing:3px;"><?php the_title() ?></span></h2>
+									<?php while ( have_posts() ) :
+			the_post();?>
+									<?php
+            $content = get_the_content();
+            ?>
+								    <?php echo Parse::ParseContent(apply_filters('the_content', $content),get_the_ID());?>
+								    <?php endwhile;?>
+								</div>
+							</div>
+						</div>
+					</div>
+					</div>
+				</div>
+				
+			</div>
+        </div>
+<?php get_footer(); ?>
